764bf4dcee6dd42ae6fa1130e78c62799b5b4a54
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-07-14 17:14:34 -0400

    Update MicroCard.js

--- src/components/MicroCard.js
+++ src/components/MicroCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MicroCard = ({ user }) => {
   return (
     <div className="section microcard">
-      <img loading="lazy" className="user-profile lazyload"  data-src={user.avatar} width={'100%'} />
+      <img loading="lazy" className="user-profile"  src={user.avatar} width={'100%'} />
       <div>
         <h3 className="bold">{user.username}</h3>
         <p>{user.name}</p>
