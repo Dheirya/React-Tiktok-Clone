70a4096b3c4d71988b6368e36b56547652e3ab82
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-07-14 17:15:05 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,26 +11,21 @@ const Home = () => {
   let topFiveNotFollowing
   let topFiveFollowing
 
-  //auto populating with dummy data
   const addData = async () => {
     await axios.post('/.netlify/functions/addData')
   }
 
-  //fetch all the tik-tok posts to your feed
   const fetchData = async () => {
     const results = await axios.get('/.netlify/functions/posts')
-    console.log(results.data)
     setUsers(results.data)
   }
 
-  //toggle user from followed to unfollowed
   if (userToToggle) {
   const newValue = userToToggle.is_followed ? false : true
   const data = {is_followed: newValue}
 
    axios.put('/.netlify/functions/edit', {userId: userToToggle.id, data: data })
     .then(res => res.json())
-    .then(json => console.log(json))
     .catch(err => console.error('error:' + err))
     .then(() => fetchData())
       setUserToToggle(null)
@@ -56,7 +51,9 @@ const Home = () => {
     <>
     {descendingUsers && (
       <div className='container'>
-        <FollowersColumn users={topFiveFollowing} />
+        <div className='fivefollowing'>
+            <FollowersColumn users={topFiveFollowing} />
+        </div>
         <div className='feed'>
           {descendingUsers.map((descendingUser, index) => (
             <Card
@@ -72,7 +69,7 @@ const Home = () => {
               <h2 className="bold">Suggested accounts</h2>
               <div className="break" />
               {topFiveNotFollowing.map((notFollowingUser, index) => (
-                <MiniCard 
+                <MiniCard
                   key={index} user={notFollowingUser}
                   toggleFollow={userToToggle => setUserToToggle(userToToggle)}
 		  	        />)
