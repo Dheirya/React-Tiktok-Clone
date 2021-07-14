8bd3aa43931c6977ec8e7330e31dd868d5807290
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-07-14 17:11:21 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,37 +3,51 @@ import React from 'react'
 const Card = ({ user, toggleFollow }) => {
 
   const timestamp = user.timestamp
-  const timeStampReformat = timestamp.slice(2, 7)
+  const timeStampReformat = timestamp.slice(2, 10).replaceAll("-", "/");
+  const timeArr = timeStampReformat.split("/").reverse();
+  const timemonth = timeArr[1];
+  const sorted = timeArr.sort(function(x,y){ return x === timemonth ? -1 : y === timemonth ? 1 : 0; });
+  const finaltime = sorted.join('/');
 
   return (
     <div className="card">
       <div className="break" />
         <div className="section">
           <div className="user-info">
-            <img className="user-profile"  src={user.avatar} width={'100%'} />
+            <img loading="lazy" className="user-profile lazyload"  data-src={user.avatar} width={'100%'} />
             <div>
             <div className="section">
-              <h3 className="bold">{user.username}</h3>
-              <p className="username">{user.name}</p>
-              <p>{timeStampReformat}</p>
-            </div> 
+              <h2 className="bold">{user.username}</h2>
+              <p className="username" style={{marginTop: "16px"}}>@{user.name}</p>
+              <p style={{marginTop: "16px", marginLeft: "-7px"}}>on {finaltime}</p>
+            </div>
             <p>{user.caption}</p>
             </div>
           </div>
-          {user.button_visible && <div className={user.is_followed? "followed-button" : "follow-button"}
+          {user.button_visible && <div id="followbtnpost" className={user.is_followed? "followed-button" : "follow-button"}
             onClick={() => toggleFollow(user)}>
-              {user.is_followed? "Following": "Follow"}
+              {user.is_followed? "Unfollow": "Follow"}
           </div>}
         </div>
-        <video className="video" controls>
+        <video preload="eager" style={{textAlign: 'left'}} className="video" controls>
           <source src={user.video} type="video/mp4" />
         </video>
-        <div className="section socials">
-           <i class="far fa-heart"></i>
-          <div className="social-tag">{user.likes}</div>
-          <i class="far fa-comment-dots"></i>
-          <div className="social-tag">{user.comments}</div>
-          <i class="far fa-share-square"></i>
+        <div className="socials">
+          <div className="heart">
+              <i className="fa fa-heart"></i>
+              <br/>
+              <div className="social-tag">{user.likes}</div>
+              <br/>
+          </div>
+          <div className="comment">
+              <i className="fa fa-comment"></i>
+              <br/>
+              <div className="social-tag">{user.comments}</div>
+              <br/>
+          </div>
+          <div className="share">
+            <i className="fa fa-share"></i>
+          </div>
         </div>
     </div>
   )
