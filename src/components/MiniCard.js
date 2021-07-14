248692caa4fb98a564e320ec1e783945de2a1b14
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-07-14 17:12:11 -0400

    Update MiniCard.js

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -4,19 +4,19 @@ const MiniCard = ({ user, toggleFollow }) => {
   return (
     <div className="section minicard">
       <div className="section">
-        <img className="user-profile"  src={user.avatar} width={'100%'} />
+        <img loading="lazy" className="user-profile lazyload"  data-src={user.avatar} width={'100%'} />
         <div>
           <h3 className="bold">{user.username}</h3>
           <p>{user.name}</p>
       </div>
     </div>
-      {user.button_visible && <div className={user.is_followed? "followed-button" : "follow-button"}
+      {user.button_visible && <div id="follow-mini" className={user.is_followed? "followed-button" : "follow-button"}
         onClick={() => toggleFollow(user)}
       >
-      {user.is_followed? "Following": "Follow"}
+      {user.is_followed? "Unfollow": "Follow"}
       </div>}
     </div>
   )
 }
-    
+
 export default MiniCard
