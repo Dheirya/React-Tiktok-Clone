96baf18e0b9747ab800922ef6862a679585ef6b3
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-07-14 17:11:47 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className="header">
-      <Link to='/'>
-        <div className="logo"></div>
+      <Link to='/' style={{textDecoration: 'none'}}>
+        <span class="texte" data-text="&nbsp;React Tok">&nbsp;React Tok</span>
       </Link>
       <div className="upload-container">
         <div className="section">
           <Link to='/upload'>
             <div className="upload" />
           </Link>
-          <img className="personal" src="https://i.imgur.com/QwZod6m.png" />
+          <img loading="lazy" className="personal lazyload" data-src="https://i.imgur.com/QwZod6m.png" />
         </div>
       </div>
     </div>
